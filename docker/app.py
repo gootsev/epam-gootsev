d06b287179940d4fd5279b4fac2f94206e20795f
Gootsev Vitaliy <gootsev@gmail.com>
2021-08-30 03:23:35 +0300

    Docker-compose added

--- docker/app.py
+++ docker/app.py
@@ -1,33 +1,38 @@
 from flask import Flask
-import requests, json, os
+import requests, json, os, psycopg2
 
 app = Flask(__name__)
 
+name = os.environ['NAME']
+city = os.environ['CITY']
+api = os.environ['API_KEY']
+
+
+
 @app.route('/')
 def main_app():
-
-	if os.environ['NAME'] == "none":
-		name = "Max Payne"
-	else:
-		name = os.environ['NAME']
-
-	if os.environ['CITY'] == "none":
-		city = "Moscow"
-	else:
-		city = os.environ['CITY']
-
-	if os.environ['API_KEY'] == "none":
-		api = "a1311b63a19a3dd611a43a3dbec666b5"
-	else:
-		api = os.environ['API_KEY']
-	
-
 	params = {'q': {city}, 'appid': {api}}
 	request = requests.get("https://api.openweathermap.org/data/2.5/weather?units=metric", params=params)
 	description = request.json()["weather"][0]["description"]
 	temp = request.json()['main']['temp']
+	db_connect(temp)
+	return f"Hi {name}! It's {temp} degrees in {city} now, {description}."
 
-	return f"Hi {name}! Now in {city} {temp} C, {description}."
+def db_connect(temp):
+	connection = psycopg2.connect(user='postgres', password='postgres', database='postgres', host="db")
+	try:
+		cursor = connection.cursor()
+		cursor.execute('CREATE TABLE IF NOT EXISTS weather (id SERIAL PRIMARY KEY, VALUE VARCHAR(255) NOT NULL)')
+		sql = f"INSERT INTO weather (value) VALUES ( {temp} )"
+		cursor.execute(sql)
+		connection.commit()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print("Error while connecting to PostgreSQL", error)
+	finally:
+		if(connection):
+			cursor.close()
+			connection.close()
+        
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
